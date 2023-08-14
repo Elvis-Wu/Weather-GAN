f11b8630b7942c3c4f497da471fc9ecd3a664631
Elvis-Wu <j91002@gmail.com>
2023-08-14 18:18:23 +0800

    can load data

--- Load_data.py
+++ Load_data.py
@@ -1,5 +1,53 @@
-import tensorflow as tf
+from torch.utils.data import Dataset, DataLoader
+from torchvision import datasets
+from torchvision import transforms
+from torchvision.io import read_image
+import numpy as np
+import os
+import pandas as pd #資料處理
+import torch
+from PIL import Image
 
-gpus = tf.config.experimental.list_physical_devices('GPU')
-for gpu in gpus :
-    tf.config.experimental.set_memory_growth(gpu, True)
+# 設定dataset路徑
+project_path = r"D:\NTUT\Weather_AI\self-project\GAN\weather-GAN"
+train_data_path = str(project_path + r"\train_data")
+output_data_path = str(project_path + r"output_data")
+
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    #transforms.Resize((256, 256)),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+class My_Dataset(Dataset) :
+    def __init__(self, path, transform=None) :
+        super().__init__()
+        self.image_files = os.listdir(path)
+        self.list_files = []
+        for file in self.image_files :
+            self.list_files.append(os.path.join(path, file))
+
+    # Indicate the total size of the dataset
+    def __len__(self) :
+        return len(self.image_files)
+    
+    # 1. Read from file (using numpy.fromfile, PIL.Image.open)
+    # 2. Preprocess the data (torchvision.Transform).
+    # 3. Return the data (e.g. image and label)
+    def __getitem__(self, index) :
+        image = Image.open(self.list_files[index]).convert('RGB')
+        if transform is None :
+            return image
+        else :
+            image = transform(image)
+            return image
+
+#讀取圖片，一次四張
+if __name__ == '__main__' :
+    #path_image = r"D:\NTUT\Weather_AI\self-project\GAN\weather-GAN\train_data"
+    path_image = train_data_path
+    train_data = My_Dataset(path_image, transform=transform)
+    train_dataloader = DataLoader(train_data, batch_size=4, shuffle=True)
+    for image in train_dataloader :
+        print(image.shape)
+        #4(張), 3(RGB三通道), 3600(長), 3600(寬)
+        break
